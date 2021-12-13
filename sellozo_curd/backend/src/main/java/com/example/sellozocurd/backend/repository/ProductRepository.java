ff910c605de913cbc53881fe336ea23eadf30f41
Mohammad M Alam <malam004@gmail.com>
2021-12-12 23:01:10 -0600

    Delete sellozo_curd directory

--- sellozo_curd/backend/src/main/java/com/example/sellozocurd/backend/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sellozocurd.backend.repository;
-
-import com.example.sellozocurd.backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByProductName(String productName);
-}
